9549f7a6bf7aad7a2d6353403267a3765fccd5f5
cicada1208 <plumes@cycu.org.tw>
2023-02-07 00:13:08 +0800

    env VITE_APP_RUN

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
-import "./assets/main.css";
+import './assets/main.css';
+
+console.log('VITE_APP_RUN:', import.meta.env.VITE_APP_RUN);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');

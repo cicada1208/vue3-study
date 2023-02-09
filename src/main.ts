ab828e7257af2d2ea72889047c674c0145524052
cicada1208 <plumes@cycu.org.tw>
2023-02-09 23:49:24 +0800

    process.env.NODE_ENV

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import router from './router';
 import './assets/main.css';
 
 console.log('VITE_APP_RUN:', import.meta.env.VITE_APP_RUN);
+// import.meta.env.MODE = process.env.NODE_ENV
+console.log('import.meta.env.MODE:', import.meta.env.MODE);
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 const app = createApp(App);
 

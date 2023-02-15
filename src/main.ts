4ff76f45ee78b9ebed308e56acf4754bcaf00a2d
cicada1208 <plumes@cycu.org.tw>
2023-02-16 00:08:13 +0800

    import.meta.url

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
 import App from './App.vue';
 import router from './router';
-
 import './assets/main.css';
 
 console.log('VITE_APP_RUN:', import.meta.env.VITE_APP_RUN);
 // import.meta.env.MODE = process.env.NODE_ENV
 console.log('import.meta.env.MODE:', import.meta.env.MODE);
 console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('import.meta.url:', import.meta.url); // http://localhost:5173/src/main.ts?t=1676475192919
 
 const app = createApp(App);
 

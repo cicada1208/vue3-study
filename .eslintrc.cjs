87a1f8a4818b3d508f5263fc9912887612e212e2
cicada1208 <plumes@cycu.org.tw>
2023-02-19 12:25:28 +0800

    .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,23 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
-    // '@vue/eslint-config-prettier'
-  ],
   parserOptions: {
     ecmaVersion: 'latest',
     ecmaFeatures: {
       jsx: true // If you are using JSX, you need to enable JSX in your ESLint configuration.
     }
+  },
+  extends: [
+    // add more generic rulesets here
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript'
+    // '@vue/eslint-config-prettier'
+  ],
+  rules: {
+    // override/add rules settings here
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': ['off', 'windows'] // 關閉 win os 對換行的 lint
   }
 };

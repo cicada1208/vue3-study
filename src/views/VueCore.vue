cc37f3681bc1e59125de9e3224d32e8a268cbeaf
cicada1208 <plumes@cycu.org.tw>
2023-03-05 01:36:02 +0800

    computed

--- src/views/VueCore.vue
+++ src/views/VueCore.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
+import { reactive, ref, computed } from 'vue';
 
 // 创建响应式对象或数组
 // 仅对对象类型有效（对象、数组和 Map、Set 这样的集合类型），而对 string、number 和 boolean 这样的 原始类型 无效。
@@ -15,6 +15,11 @@ const refState = ref({ count: 0 });
 function refStateIncrement() {
   refState.value.count++;
 }
+
+// 計算函數中不應變更原數組，需先建立副本再行操作
+// 會變更原數組的分法：pop()、push()、shift()、unshift()、splice()、sort()、reverse()
+const numbers = reactive([1, 2, 3, 4, 5]);
+const reverseNumbers = computed(() => [...numbers].reverse());
 </script>
 
 <template>
@@ -29,6 +34,11 @@ function refStateIncrement() {
       {{ refState.count }}
       <!-- 若 refState 為顶层属性，无需 .value -->
     </button>
+
+    <h2>computed</h2>
+    <ul>
+      <li v-for="num in reverseNumbers" :key="num">{{ num }}</li>
+    </ul>
   </div>
 </template>
 

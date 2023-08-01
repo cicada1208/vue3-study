52f8e28c13230e79187a19b03d57523a9d9934c7
cicada1208 <plumes@cycu.org.tw>
2023-08-01 23:43:00 +0800

    throttleFilter

--- src/views/VueUseEx.vue
+++ src/views/VueUseEx.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { useMouse, useTitle } from '@vueuse/core';
+import { useMouse, useTitle, throttleFilter } from '@vueuse/core';
 import dayjs from 'dayjs';
 
 // tracks mouse position
-const { x, y } = useMouse();
+// 並使用 throttleFilter 1s 更新1次
+const { x, y } = useMouse({ eventFilter: throttleFilter(1000) });
 
 // Reactive document title.
 const title = useTitle();

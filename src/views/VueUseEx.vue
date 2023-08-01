88308add888770a6e1d2cc1d11abcd30be8401c1
cicada1208 <plumes@cycu.org.tw>
2023-08-01 11:34:05 +0800

    useTitle

--- src/views/VueUseEx.vue
+++ src/views/VueUseEx.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
-import { useMouse } from '@vueuse/core';
+import { useMouse, useTitle } from '@vueuse/core';
+import dayjs from 'dayjs';
 
 // tracks mouse position
 const { x, y } = useMouse();
+
+// Reactive document title.
+const title = useTitle();
+function changeTitle() {
+  title.value = dayjs().format('YYYY-MM-DD HH:mm:ss');
+}
 </script>
 
 <template>
   <div>
     <h2 id="useMouse"><a href="#useMouse">useMouse</a></h2>
     {{ `x = ${x}, y = ${y}` }}
+
+    <h2 id="useTitle"><a href="#useTitle">useTitle</a></h2>
+    {{ title }}
+    <button @click="changeTitle">changeTitle</button>
   </div>
 </template>
 

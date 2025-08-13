ed15a7c51ce203e2c8d36b75ec99cac2b1978719
cicada1208 <plumes@cycu.org.tw>
2025-08-14 00:22:09 +0800

    modify comment for env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -2,13 +2,13 @@
 // triple-slash directive: 作用是告訴 TypeScript 去讀取 vite/client 的型別宣告，讓 TS 知道 Vite 預設提供的一些全域型別
 
 // 全域型別宣告檔(.d.ts)：
-// 作用是幫 TypeScript 認識專案中 TS 預設不認識的東西
-// 不需在每個檔案 import 型別
-// 只要檔案在 tsconfig.json 的 include 範圍內，便會生效
+// 作用是幫 TypeScript 認識專案中預設不認識的檔案
+// 可在專案任意檔案中直接使用型別，而不用 import
+// .d.ts 只要在 tsconfig.json 的 include 範圍內，便會生效
 
 /**
  * - import.meta.env 物件型別
- * - 可擴充定義 .env 裡的變數
+ * - 可擴展定義 .env 裡的變數
  */
 interface ImportMetaEnv {
   readonly VITE_APP_RUN: string;

a527980c4964bf6b7ec9df47eded3ad5b183b537
cicada1208 <plumes@cycu.org.tw>
2025-08-15 00:44:46 +0800

    modify comment from env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,12 +3,12 @@
 
 // 全域型別宣告檔(.d.ts)：
 // 作用是幫 TypeScript 認識專案中預設不認識的檔案
-// 可在專案任意檔案中直接使用型別，而不用 import
-// .d.ts 只要在 tsconfig.json 的 include 範圍內，便會生效
+// 可在專案任意檔案中直接使用全域型別，而不用 import
+// .d.ts 只要在 tsconfig.json include 範圍內，便會生效
 
 /**
  * - import.meta.env 物件型別
- * - 可擴展定義 .env 裡的變數
+ * - 可擴展定義 .env 檔案裡 VITE_ 開頭環境參數
  */
 interface ImportMetaEnv {
   readonly VITE_APP_RUN: string;

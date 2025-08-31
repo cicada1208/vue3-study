f6304ff4387033492fcb441943783ee8b32f898c
cicada1208 <plumes@cycu.org.tw>
2025-08-31 16:35:55 +0800

    vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vueDevTools(), vue(), vueJsx()],
   resolve: {
     alias: {
       '@': new URL('./src', import.meta.url).pathname

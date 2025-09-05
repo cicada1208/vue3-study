2045951e41cc68c98d081b1988249c3f132a0943
cicada1208 <plumes@cycu.org.tw>
2025-09-06 01:07:34 +0800

    pinia

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,7 +1,9 @@
-import { ref, computed } from "vue";
-import { defineStore } from "pinia";
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
 
-export const useCounterStore = defineStore("counter", () => {
+// 推薦命名方式：useXXXStore
+// 調用 useCounterStore()，全域狀態 store 實例才會被創建
+export const useCounterStore = defineStore('counter', () => {
   const count = ref(0);
   const doubleCount = computed(() => count.value * 2);
   function increment() {
